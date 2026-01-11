196be5fdc70c160b223f1bcb3563b5d61fee4c3c
Polin-a152 <gmisetov60@gmail.com>
2026-01-11 15:47:11 +0300

    Update filters.h

--- filters.h
+++ filters.h
@@ -7,6 +7,12 @@ Image *filter_crop(const Image *in, int w, int h);
 Image *filter_gs(const Image *in);
 Image *filter_neg(const Image *in);
 Image *filter_sharp(const Image *in);
+Image *filter_edge(const Image *in, double threshold);
+Image *filter_med(const Image *in, int window);
+Image *filter_blur(const Image *in, double sigma);
+
+Image *filter_crys(const Image *in, int size);
+Image *filter_glass(const Image *in, int radius);
 
 
 
